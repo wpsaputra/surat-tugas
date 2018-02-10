4755059c0cf5bec74b1d4f1782a4ce2404ebedf5
wayanpermana <aco.wayan@outlook.com>
2018-02-10 11:26:20 +0800

    add klorofil css

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -24,8 +24,12 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
     ];
+    // public $depends = [
+    //     'yii\web\YiiAsset',
+    //     'yii\bootstrap\BootstrapAsset',
+    // ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
--- views/layouts/main.2.php
+++ views/layouts/main.2.php
@@ -1,43 +1,39 @@
 <?php
 
-use yii\helpers\Url;
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use yii\helpers\Url;
 
 AppAsset::register($this);
-$this->registerJsFile(
-    '@web/js/klorofil-common.js',
-    ['depends' => [\yii\web\JqueryAsset::className()]]
-);
-
 ?>
-
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="<?= Yii::$app->language ?>">
 <head>
-	<title>Dashboard | Klorofil - Free Bootstrap Dashboard Template</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="<?= Yii::$app->charset ?>">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-	<!-- VENDOR CSS -->
-	<link rel="stylesheet" href="<?php echo Url::to('@web/vendor/bootstrap/css/bootstrap.min.css');?>">
-	<link rel="stylesheet" href="<?php echo Url::to('@web/vendor/font-awesome/css/font-awesome.min.css');?>">
-	<link rel="stylesheet" href="<?php echo Url::to('@web/vendor/linearicons/style.css');?>">
-	<!-- <link rel="stylesheet" href="assets/vendor/chartist/css/chartist-custom.css"> -->
-	<!-- MAIN CSS -->
-	<link rel="stylesheet" href="<?php echo Url::to('@web/css/main.css');?>">
-	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
-	<!-- <link rel="stylesheet" href="assets/css/demo.css"> -->
-	<!-- GOOGLE FONTS -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="<?php echo Url::to('@web/vendor/bootstrap/css/bootstrap.min.css');?>">
+    <link rel="stylesheet" href="<?php echo Url::to('@web/css/main.css');?>">
+    <link rel="stylesheet" href="<?php echo Url::to('@web/vendor/font-awesome/css/font-awesome.min.css');?>">
+    <link rel="stylesheet" href="<?php echo Url::to('@web/vendor/linearicons/style.css');?>">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
 </head>
 <body>
-    <?php $this->beginBody() ?>
-	<!-- WRAPPER -->
+<?php $this->beginBody() ?>
+
+<!-- WRAPPER -->
 	<div id="wrapper">
 		<!-- NAVBAR -->
 		<nav class="navbar navbar-default navbar-fixed-top">
@@ -497,13 +493,16 @@ $this->registerJsFile(
 	</div>
 	<!-- END WRAPPER -->
 	<!-- Javascript -->
-	<script src="assets/vendor/jquery/jquery.min.js"></script>
-	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<!-- <script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script> -->
+	<!-- <script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script> -->
+	
+    <!-- <script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script> -->
 	<!-- <script src="assets/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
 	<script src="assets/vendor/chartist/js/chartist.min.js"></script> -->
 	<script src="<?php echo Url::to('@web/js/klorofil-common.js');?>"></script>
-    <?php $this->endBody() ?>
+	<!-- <script src="assets/scripts/klorofil-common.js"></script> -->
+
+<?php $this->endBody() ?>
 </body>
 </html>
 <?php $this->endPage() ?>
