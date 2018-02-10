577e07dd87780ba02fd3a1ae9b8c5a2e492214cf
wayanpermana <aco.wayan@outlook.com>
2018-02-11 07:10:07 +0800

    before relayout form

--- views/kwitansi/update.php
+++ views/kwitansi/update.php
@@ -9,17 +9,42 @@ $this->title = 'Update Kwitansi: '.$model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Kwitansis', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
+
+$this->registerJsFile(
+    '@web/vendor/ckeditor/ckeditor.js',
+    ['depends' => [\yii\web\JqueryAsset::className()]]
+);
+
+$this->registerJsFile(
+    '@web/js/kwitansi.js',
+    ['depends' => [\yii\web\JqueryAsset::className()]]
+);
 ?>
 <div class="kwitansi-update">
-    <!-- INPUTS -->
-    <div class="panel">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-md-5">
+            <!-- INPUTS -->
+            <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+                </div>
+                <div class="panel-body">
+                <?= $this->render('_form', [
+                    'model' => $model,
+                ]) ?>
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-        <?= $this->render('_form', [
-            'model' => $model,
-        ]) ?>
+        <div class="col-md-7">
+            <!-- INPUTS -->
+            <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Preview</h3>
+                </div>
+                <div class="panel-body">
+                    <textarea name="editor1" id=editor1></textarea>
+                </div>
+            </div>
         </div>
     </div>
 
