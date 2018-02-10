c749b545e701f309081c44e8fa92a18190bd815b
wayanpermana <aco.wayan@outlook.com>
2018-02-10 17:48:07 +0800

    before add ck editor

--- views/kwitansi/create.php
+++ views/kwitansi/create.php
@@ -11,16 +11,32 @@ $this->params['breadcrumbs'][] = ['label' => 'Kwitansis', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="kwitansi-create">
-    <!-- INPUTS -->
-    <div class="panel">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-md-6">
+            <!-- INPUTS -->
+            <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+                </div>
+                <div class="panel-body">
+                <?= $this->render('_form', [
+                    'model' => $model,
+                ]) ?>
+                </div>
+            </div>
         </div>
-        <div class="panel-body">
-        <?= $this->render('_form', [
-            'model' => $model,
-        ]) ?>
+        <div class="col-md-6">
+            <!-- INPUTS -->
+            <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Preview</h3>
+                </div>
+                <div class="panel-body">
+                </div>
+            </div>
         </div>
     </div>
+
+    
 
 </div>
